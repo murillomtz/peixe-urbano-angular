8ffae549558d83482fcc3318d4e373a83b95a614
Murillo Santos <murillo.santos@ucsal.edu.br>
2021-04-20 15:10:03 -0300

    Criação de service e iniciando Promise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopoComponent } from './topo/topo.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    HomeComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import {Oferta} from './../shared/oferta.models';
+import {OfertasServices} from './../services/ofertas.services';
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasServices]
+})
+export class HomeComponent implements OnInit {
+
+  public ofertas: Oferta[];
+
+  constructor(private offerService: OfertasServices) {
+  }
+
+  ngOnInit(): void {
+    //this.offers = this.offerService.getOfertas();
+    // console.log(this.offers);
+    this.offerService.getOfertas2()
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas;
+      });
+  }
+
+}
