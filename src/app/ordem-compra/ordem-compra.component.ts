258d3bdc3c0823f8cb7ecb563a1bb706fe34e653
Murillo Santos <murillo.santos@ucsal.edu.br>
2021-04-30 08:13:05 -0300

    Add forms 'ordem-compra'

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -12,6 +12,18 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string = ''
   public formaPagamento: string = ''
 
+  // criando os atributos de controle de validacao dos campos
+  public enderecoValido: boolean
+  public numeroValido: boolean
+  public complementoValido: boolean
+  public formaPagamentoValido: boolean
+
+  // estados primitivos dos campos (pristine)
+  public enderecoEstadoPrimitivo: boolean = true
+  public numeroEstadoPrimitivo: boolean = true
+  public complementoEstadoPrimitivo: boolean = true
+  public formaPagamentoEstadoPrimitivo: boolean = true
+
   constructor() { }
 
   ngOnInit(): void {
@@ -19,22 +31,53 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco
-    console.log(this.endereco)
+    //console.log(this.endereco)
+
+    this.enderecoEstadoPrimitivo = false
+
+     //se a string for maior que 3
+     if (this.endereco.length > 3) {
+      this.enderecoValido = true
+    } else {
+      this.enderecoValido = false
+    }
   }
 
   public atualizaNumero(numero: string): void {
     this.numero = numero
-    console.log(this.numero)
+    //console.log(this.numero)
+
+    this.numeroEstadoPrimitivo = false 
+
+    if (this.numero.length > 0) {
+      this.numeroValido = true
+    } else {
+      this.numeroValido = false
+    }
   }
 
   public atualizaComplemento(complemento: string): void {
     this.complemento = complemento
-    console.log(this.complemento)
+    // console.log(this.complemento)
+
+    this.complementoEstadoPrimitivo = false
+
+    if(this.complemento.length > 0) {
+      this.complementoValido = true
+    }
   }
 
   public atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento =  formaPagamento
-    console.log(this.formaPagamento)
+    //console.log(this.formaPagamento)
+
+    this.formaPagamentoEstadoPrimitivo = false 
+
+    if(this.formaPagamento.length > 0 ) {
+      this.formaPagamentoValido = true
+    } else {
+      this.formaPagamentoValido = false
+    }
   }
 
 }
